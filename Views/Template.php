60e9f25c8db99138fcced039f180b03aca9892c4
GonzalezAdriana <37461405+GonzalezAdriana@users.noreply.github.com>
2018-11-07 00:11:39 -0600

    Update Views/Template.php
    template

--- Views/Template.php
+++ Views/Template.php
@@ -8,64 +8,77 @@ class Template
        ?>
         <!DOCTYPE html>
         <!------------------------------------------------------------------------------->
-        <html >
+        <html lang="en">
         <!------------------------------------------------------------------------------->
         <head>
             <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
             <link href="<?php echo URL?>/Public/css/bootstrap.css" type="text/css" rel="stylesheet">
             <link href="<?php echo URL?>/Public/css/bootstrap.css" type="text/css" rel="stylesheet">
             <link href="<?php echo URL?>/Public/css/bootstrap-reboot.css" type="text/css" rel="stylesheet">
             <link rel="stylesheet" href="<?php echo URL?>/Public/style.css">
+            <link rel="stylesheet" href="<?php echo URL?>/dist/css/bootstrap.min.css">
+            <link href="<?php echo URL?>/Public/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+            <link href="<?php echo URL?>/Public/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+            <link href="<?php echo URL?>/Public/css/grayscale.css" rel="stylesheet">
+            <link rel="stylesheet" href="<?php echo URL?>/Public/Style2.css">
+            <link rel="stylesheet" href="<?php echo URL?>/Public/EstiloLogin.css">
+
             <script type="text/javascript" src="<?php echo URL?>/Public/js/bootstrap.bundle.min.js"></script>
             <script type="text/javascript" src="<?php echo URL?>/Public/js/bootstrap.js"></script>
             <script type="text/javascript" src="<?php echo URL?>/Public/js/jquery.min.js"></script>
-
+            <script src="<?php echo URL?>/dist/js/jquery.js"></script>
+            <script src="<?php echo URL?>/dist/js/bootstrap.min.js"></script>
+            <script src="<?php echo URL?>/Public/vendor/jquery/jquery.min.js"></script>
+            <script src="<?php echo URL?>/Public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+            <script src="<?php echo URL?>/Public/vendor/jquery-easing/jquery.easing.min.js"></script>
+            <script src="<?php echo URL?>/Public/js/grayscale.min.js"></script>
     </head>
+    <body>
         <main>
 
-            <title>Turismo</title>
-        <nav class=" bg-light navbar navbar-expand-lg navbar-light fixed-top" id="mainNav" >
-            <div class="container">
-                <a   src="valle.jpg" width="20" height="20" class="navbar-brand js-scroll-trigger" href="<?php echo URL?>login" class="btn btn-primary js-scroll-trigger" id="sesion">EDOMEX</a>
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a class="nav-link js-scroll-trigger" href="#hace">Quienes somos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link js-scroll-trigger" href="#info">Informes</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link js-scroll-trigger" href="#contac">Contactanos</a>
-                        </li>
-                    </ul>
+            <title>Edomex</title>
+            <nav class=" bg navbar navbar-expand-lg navbar-light bg-dark fixed-top text-white"  >
+                <div class="container">
+                    <a class="navbar-brand js-scroll-trigger text-white" href="<?php echo URL?>login" class="btn btn-primary js-scroll-trigger " id="sesion ">EDOMEX</a>
+                    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-bars"></i>
+                    </button>
+                    <div class="collapse navbar-collapse " id="navbarResponsive">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link js-scroll-trigger text-white" href="#hace">Que hacemos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link js-scroll-trigger text-white" href="#info">Quienes somos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link js-scroll-trigger text-white" href="#contac">Contactanos</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-
-        </nav>
-
+            </nav>
         </main>
-        <!------------------------------------------------------------>
         </html>
         <?php
     }
-    public static function footer()
-    {
+
+
+    public static function footer(){
         ?>
         <footer class="container">
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-bottom">
                 <div class="container">
-                    <p class="text-left text-white">Copyright &copy; Derechos Reservados 2018</p>
+                    <p class="  text-white">Todos los derechos reservados TESVB 2018</p>
                 </div>
             </nav>
+
         </footer>
-        </html>
-<?php
 
-}
-}
+        </body>
+        <?php
 
+    }
+}
