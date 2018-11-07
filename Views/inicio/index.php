79284574c0a85180c6e9b3159f8c7a5284c6d698
Christian <isc_victoria.c@tesvb.edu.mx>
2018-11-06 19:51:28 -0600

    index

--- Views/inicio/index.php
+++ Views/inicio/index.php
@@ -19,11 +19,10 @@
                 <div class="card-body">
                     <h5 class="card-title border-bottom pb-3">El Oro<a href="#" class="float-right d-inline-flex share"><i class="fas fa-share-alt text-primary"></i></a></h5>
                     <p class="card-text">Pueblo Magico en el Estado de México, es una de las antiguas glorias mineras de esta entidad del país y aún es un imán turístico.</p>
-<<<<<<< HEAD
+
                     <a href="<?php echo URL?>oro" class="float-right">Leer más  <i class="fas fa-angle-double-right"></i></a>
-=======
-                    <a href="<?php echo URL?>Views/pueblos/el_oro.html" class="float-right">Leer más  <i class="fas fa-angle-double-right"></i></a>
->>>>>>> fcf29b57bae5c59f9c5628ad2b66a7e09d5ad46c
+
+
                 </div>
             </div>
         </div>
@@ -65,11 +64,10 @@
                 <div class="card-body">
                     <h5 class="card-title border-bottom pb-3">Aculco <a href="#" class="float-right d-inline-flex share"><i class="fas fa-share-alt text-primary"></i></a></h5>
                     <p class="card-text">Pueblo Magico en el Estado de México, este pintoresco pueblo tiene la característica de tener muy cercano todavía a habitantes de la cultura Otomi.</p>
-<<<<<<< HEAD
+
                     <a href="<?php echo URL?>Aculco" class="float-right">Leer más <i class="fas fa-angle-double-right"></i></a>
-=======
-                    <a href="<?php echo URL?>Views/pueblos/aculco.html" class="float-right">Leer más <i class="fas fa-angle-double-right"></i></a>
->>>>>>> fcf29b57bae5c59f9c5628ad2b66a7e09d5ad46c
+
+
                 </div>
             </div>
         </div>
@@ -81,11 +79,10 @@
                 <div class="card-body">
                     <h5 class="card-title border-bottom pb-3">Ixtapan de la Sal<a href="#" class="float-right d-inline-flex share"><i class="fas fa-share-alt text-primary"></i></a></h5>
                     <p class="card-text">Magico en el Estado de México, debido a su templado clima, y a sus manantiales de aguas termales con propiedades curativas, este lugar ha sido sitio propicio para el desarrollo de balnearios.</p>
-<<<<<<< HEAD
-                    <a ref="<?php echo URL?>Ixtapan" class="float-right">Leer más  <i class="fas fa-angle-double-right"></i></a>
-=======
-                    <a href="<?php echo URL?>Views/pueblos/ixtapa.html" class="float-right">Leer más  <i class="fas fa-angle-double-right"></i></a>
->>>>>>> fcf29b57bae5c59f9c5628ad2b66a7e09d5ad46c
+
+                    <a href="<?php echo URL?>Ixtapan" class="float-right">Leer más  <i class="fas fa-angle-double-right"></i></a>
+
+
                 </div>
             </div>
         </div>
@@ -95,11 +92,9 @@
                 <div class="card-body">
                     <h5 class="card-title border-bottom pb-3">Teotihuacan <a href="#" class="float-right d-inline-flex share"><i class="fas fa-share-alt text-primary"></i></a></h5>
                     <p class="card-text">Pueblo Magico en el Estado de México, tiene muchos aspectos turísticos interesantes, más allá de sus pirámides se trata de una comunidad colmada de aspectos valiosos.</p>
-<<<<<<< HEAD
-                    <a ref="<?php echo URL?>Teotihuacan" class="float-right">Leer más <i class="fas fa-angle-double-right"></i></a>
-=======
-                    <a href="<?php echo URL?>Views/pueblos/teotihuacan.html" class="float-right">Leer más <i class="fas fa-angle-double-right"></i></a>
->>>>>>> fcf29b57bae5c59f9c5628ad2b66a7e09d5ad46c
+
+                    <a href="<?php echo URL?>Teotihuacan" class="float-right">Leer más <i class="fas fa-angle-double-right"></i></a>
+
                 </div>
             </div>
         </div>
@@ -109,11 +104,9 @@
                 <div class="card-body">
                     <h5 class="card-title border-bottom pb-3">San Martin de las Piramides <a href="#" class="float-right d-inline-flex share"><i class="fas fa-share-alt text-primary"></i></a></h5>
                     <p class="card-text">San Martin de las Piramides  Pueblo Mágico del Estado de México, por el área prehispánica que se encuentra en su territorio.</p>
-<<<<<<< HEAD
-                    <a ref="<?php echo URL?>SanMartin" class="float-right">Leer más <i class="fas fa-angle-double-right"></i></a>
-=======
-                    <a href="<?php echo URL?>Views/pueblos/martin.html" class="float-right">Leer más <i class="fas fa-angle-double-right"></i></a>
->>>>>>> fcf29b57bae5c59f9c5628ad2b66a7e09d5ad46c
+
+                    <a href="<?php echo URL?>SanMartin" class="float-right">Leer más <i class="fas fa-angle-double-right"></i></a>
+
                 </div>
             </div>
         </div>
